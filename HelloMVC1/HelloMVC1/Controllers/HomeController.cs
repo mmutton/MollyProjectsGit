f6e86613f676e1ed054421589f4b0c29455df053
Molly <30417297+mmutton@users.noreply.github.com>
2017-10-04 15:25:45 -0700

    ASP.NET MVC5 first creation
    Working with MVC5, added new test to repository.

---
+++ HelloMVC1/HelloMVC1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC1.Models;
+
+namespace HelloMVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var cars = HelloModel.GetCars();
+
+            ViewBag.MollyCreatedThisValue = "Hello MVC";
+
+            return View(cars);
+        }
+    }
+}
